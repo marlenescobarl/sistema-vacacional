ba8a5a562c180282a14eeaeed01756fd137e0da5
marlenescobarl <marlenescobarlima11@gmail.com>
2021-02-06 18:57:45 -0600

    iniciando programa

---
+++ Sistemavacacional.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Sistemavacacional
+{
+    public static void main (String Args [])
+    {
+        String nombre = "";
+        int clave = 0;
+        int antiguedad = 0;
+        
+        System.out.println("Bienvenidos al Sistema vacacional CC - Company");
+
+    }
+}
