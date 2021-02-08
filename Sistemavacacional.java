0ba47e3297138fb9c1b01a351eeac1b70bf7425c
marlenescobarl <marlenescobarlima11@gmail.com>
2021-02-07 21:55:05 -0600

    Ingreso de datos

--- Sistemavacacional.java
+++ Sistemavacacional.java
@@ -3,11 +3,19 @@ public class Sistemavacacional
 {
     public static void main (String Args [])
     {
+        Scanner go = new Scanner(System.in);
         String nombre = "";
-        int clave = 0;
-        int antiguedad = 0;
+        int clave = 0, antiguedad = 0;
         
         System.out.println("Bienvenidos al Sistema vacacional CC - Company");
+        System.out.println("");
+        System.out.println("Ingrese el nombre del trabajador ");
+        nombre = go.nextLine();
+        System.out.println("Ingrese los anos de servicio del trabajador ");
+        antiguedad = go.nextInt();
+        System.out.println("Ingrese la clave del departamento del trabajador ");
+        clave = go.nextInt();
+
 
     }
 }
